ef946005f81fadcf21045ea499baa3052151521e
Lars Ivar Hatledal <laht@ntnu.no>
2021-08-26 21:02:01 +0200

    Rellocatable package (#1)

--- include/fmilibcpp/slave.hpp
+++ include/fmilibcpp/slave.hpp
@@ -30,6 +30,34 @@ public:
     virtual bool terminate() = 0;
     virtual void freeInstance() = 0;
 
+    int get_integer(value_ref vr)
+    {
+        std::vector<int> values(1);
+        get_integer({vr}, values);
+        return values.front();
+    }
+
+    double get_real(value_ref vr)
+    {
+        std::vector<double> values(1);
+        get_real({vr}, values);
+        return values.front();
+    }
+
+    std::string get_string(value_ref vr)
+    {
+        std::vector<std::string> values(1);
+        get_string({vr}, values);
+        return values.front();
+    }
+
+    bool get_boolean(value_ref vr)
+    {
+        std::vector<bool> values(1);
+        get_boolean({vr}, values);
+        return values.front();
+    }
+
     virtual bool get_integer(const std::vector<value_ref>& vr, std::vector<int>& values) = 0;
     virtual bool get_real(const std::vector<value_ref>& vr, std::vector<double>& values) = 0;
     virtual bool get_string(const std::vector<value_ref>& vr, std::vector<std::string>& values) = 0;
